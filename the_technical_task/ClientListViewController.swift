7e5e09e0a511acf777d5e113cfa8659ef1342452
Gleb Kalachev <megas781@gmail.com>
2017-11-16 12:54:41 +0300

    Alpha 1.00
    Начинаем работу в Storyboard

---
+++ the_technical_task/ClientListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  the_technical_task
+//
+//  Created by Gleb Kalachev on 11/16/17.
+//  Copyright © 2017 Gleb Kalachev. All rights reserved.
+//
+
+import UIKit
+
+class ClientListViewController: UITableViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+    }
+
+}
+
